3cbfaa301f4b9c090f34f3b422318b370bce4c65
Mykhailo <dzhammykhailo@knu.ua>
2025-02-04 10:05:27 +0200

    Додав НСД

--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
 def factorial(n):
-	res = 1
-	for i in range(1,n+1):
-		res *= i
-	return res
+    res = 1
+    for i in range(1, n + 1):
+        res *= i
+    return res
 
-def
+
+def nsd_finder(a, b):
+    while b:
+        a, b = b, a % b
+    return a
